251f27eb739c04ce331144c6a4f986a33cd9f14b
Dikshant Agarwal <dikshant.agarwal@gspann.com>
2025-08-28 16:08:56 +0530

    Update playwright.config.js with UI and API tests

--- playwright.config.js
+++ playwright.config.js
@@ -4,13 +4,13 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
 
   globalSetup: require.resolve('./utils/bootstrap.js'),
+  testDir: 'playwright-tests',
   timeout: 60_000,
   expect: { timeout: 5_000 },
   retries: process.env.CI ? 2 : 0,
   reporter: [
     ['list'],
-    ['allure-playwright', { outputFolder: 'allure-results' }],
-    [require.resolve('./utils/reporter-plugin.js')] 
+    ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
   use: {
     headless: true,
@@ -19,12 +19,15 @@ export default defineConfig({
     baseURL: process.env.BASE_URL || 'https://your-web-app.com'
   },
   projects: [
+    // API project – only API folder
     {
       name: 'api',
+      testDir: 'playwright-tests/api/spec',
       testMatch: /.*\/api\/.*\.spec\.ts/,   // only run tests under /api
     },
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox',  use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit',   use: { ...devices['Desktop Safari'] } },
+    // UI projects – only UI folder
+    { name: 'chromium', testDir: 'playwright-tests/ui/specs', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox',  testDir: 'playwright-tests/ui/specs', use: { ...devices['Desktop Firefox'] } },
+    { name: 'webkit',   testDir: 'playwright-tests/ui/specs', use: { ...devices['Desktop Safari'] } },
   ],
 });
