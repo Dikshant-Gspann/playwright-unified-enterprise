279e26df2a2c80fc907636330602595a97ed4084
Dikshant Agarwal <dikshant.agarwal@gspann.com>
2025-08-28 12:38:31 +0530

    Update bootstrap.js

--- utils/bootstrap.js
+++ utils/bootstrap.js
@@ -1,11 +1,10 @@
-#!/usr/bin/env node
-// Step 1: hook TS loader
-require('ts-node').register({
-  project: require.resolve('../tsconfig.json'),
-});
-
-// Step 2: import and run your TS code
+// utils/bootstrap.js
 const { setupAll } = require('./plugin-runner');
+
+// 1) a global task queue that plugins can push to
+global.__postRunTasks = [];
+
+// 2) Playwright globalSetup
 module.exports = async () => {
-  await setupAll();
+  await setupAll(); // plugins will register tasks here
 };
