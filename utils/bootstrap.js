5fc43d4b5e40c771e6a276de700d57f60c098e21
Dikshant Agarwal <dikshant.agarwal@gspann.com>
2025-08-28 17:39:56 +0530

    Update bootstrap.js

--- utils/bootstrap.js
+++ utils/bootstrap.js
@@ -1,10 +1,4 @@
 // utils/bootstrap.js
 const { setupAll } = require('./plugin-runner');
-
-// 1) a global task queue that plugins can push to
-global.__postRunTasks = [];
-
-// 2) Playwright globalSetup
-module.exports = async () => {
-  await setupAll(); // plugins will register tasks here
-};
+global.__postRunTasks = [];        // <— create the queue first
+module.exports = async () => { await setupAll(); };
